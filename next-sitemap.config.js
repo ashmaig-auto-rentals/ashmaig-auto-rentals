bca2530ea07e9e432c1e661c243a97e611546024
AhmedAshmaig <cleanzol123@gmail.com>
2025-09-15 21:51:18 -0700

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ashmaigautorentals.com', // change to your domain
+    generateRobotsTxt: true, // generates robots.txt too
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 0.7,
+  };
